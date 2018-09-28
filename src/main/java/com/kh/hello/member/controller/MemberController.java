a6f0af081ee64aef7043074b0bf4de4db911f3fd
user1 <user1@KH_B>
2018-09-28 15:12:52 +0900

    레알 타겟 안올렸어요ㅜㅜ

--- src/main/java/com/kh/hello/member/controller/MemberController.java
+++ src/main/java/com/kh/hello/member/controller/MemberController.java
@@ -20,88 +20,88 @@ public class MemberController {
 	@Autowired
 	private MemberService ms;
 	@Autowired BCryptPasswordEncoder passwordEncoder;
-	
 
-	
+
+
 	@RequestMapping(value ="insertUser.me")
 	public String insertMember(Model model,Member m ){
-	
+
 		String encPassword = passwordEncoder.encode(m.getPassword());
-		
-/*		System.out.println("암호화 : "+encPassword);*/
+
+		/*		System.out.println("암호화 : "+encPassword);*/
 		m.setPassword(encPassword);
-		
-		
+
+
 		int result = ms.insertMember(m);
 		
+		//유저시퀀스
 		int mid = ms.selectMemberSequence();
-		
+
 		if(result >0){
-			
+
 			System.out.println("mid : " + mid);
-			
+
 			model.addAttribute("mid", mid);
-			
+
 			return "member/addUserInfo";
-			
+
 		}else{
 			System.out.println("실패");
 			return "main/main";
 		}
-	
-}
-	
+
+	}
+
 	@RequestMapping(value="insertSeller.me")
 	public String insertSeller(Model model, Member m){
-		
+
 		String encPassword = passwordEncoder.encode(m.getPassword());
-		
+
 		/*System.out.println(m);*/
-		
+
 		int result = ms.insertSeller(m);
-		
+		//int mid  = ms.
+
 		if(result > 0){
-			return "main/main";
+			return "member/addSellerInfo";
 		}
-		
 		return null;
-		
-		
 	}
-	
+
 	@RequestMapping(value="login.me")
 	public String loginUser(Model model, Member m){
-		
+
 		try {
 			model.addAttribute("loginUser", ms.loginMember(m));
 			System.out.println("로그인 성공");
-			
+
 			return "main/main";
 		} catch (LoginException e) {
 			model.addAttribute("msg",e.getMessage());
-		
+
 			return "common/errorPage";
 		}
 	}
 	@RequestMapping(value="addUser.me")
 	public String updateAddUser(Model model, Member m){
-		
+
 		System.out.println("m : 이거다"+m);
-		
-		
+
+
 		int result = ms.updateAddUser(m);
-		
+
 		if(result >0){
 			return "main/main";
-			
-			
+
+
 		}else{
 			return"common/errorPage";
-			
+
 		}
-		
-		
-		
+
+
+
 	}
-	
+	//@RequestMapping(value="addSeller")
+
 }
--- src/main/webapp/WEB-INF/views/common/menubar.jsp
+++ src/main/webapp/WEB-INF/views/common/menubar.jsp
@@ -35,13 +35,12 @@
 		border: none;
 	}
 	
-	.loginBtn {
+	.loginBtn1 {
 		width: 300px;
 		height: 50px;
 		margin-top: 10px;
 		border: none;
 	}
-	
 	#snsBtn {
 		margin-left: auto;
 		margin-right: auto;
@@ -223,8 +222,8 @@
 			<label style="width: 180px; font-size: 30px;">Hello Korea</label>
 		</div>
 		<div id="snsBtn">
-			<button class="loginBtn">Facebook</button>
-			<button class="loginBtn">Google</button>
+			<button class="loginBtn1">Facebook</button>
+			<button class="loginBtn1">Google</button>
 		</div>
 		<hr style="	width: 400px;
 		border: solid #D8D8D8 1px;">
@@ -254,7 +253,7 @@
 			</table>
 			<hr style="	width: 400px;border: solid #D8D8D8 1px;">
 			<div align="center">
-				<button class="loginBtn">로그인</button>
+				<button class="loginBtn1">로그인</button>
 			</div>
 		</form>
 		<div align="center">
@@ -289,7 +288,7 @@
 					</tr>
 				</table>
 				<div align="center">
-					<button class="loginBtn">회원가입하기</button>
+					<button class="loginBtn1">회원가입하기</button>
 				</div>
 			</form>
 			<div align="center">
@@ -338,7 +337,7 @@
 				</tr>
 			</table>
 			<div align="center">
-				<button class="loginBtn">판매자회원가입</button>
+				<button class="loginBtn1">판매자회원가입</button>
 			</div>
 		</form>
 	</div>
