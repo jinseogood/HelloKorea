e9eaeb4659ae97069105cbcddf3ae8058908fca0
수정 <@>
2018-09-28 15:00:01 +0900

    zdasdas

--- src/main/java/com/kh/hello/board/controller/BoardPageController.java
+++ src/main/java/com/kh/hello/board/controller/BoardPageController.java
@@ -1,12 +1,28 @@
 package com.kh.hello.board.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.hello.board.FileUpload;
+import com.kh.hello.board.Response;
 
 @Controller
 public class BoardPageController {
-	@RequestMapping("reviewWrite.bo")
-	public String reviewWrite(){
+	@RequestMapping(value = "reviewWrite.bo")
+	public String reviewWrite(Model model, HttpServletRequest request/*, @RequestParam("file") MultipartFile[] file*/){
+		
+		model.addAttribute("msg", "가냐");
+		
 		return "board/reviewWrite";
 	}
 	
@@ -15,8 +31,22 @@ public class BoardPageController {
 		return "common/reportWrite";
 	}
 	
-	@RequestMapping("starTest.bo")
+	/*@RequestMapping("starTest.bo")
 	public String starTest(){
 		return "board/test";
-	}
+	}*/
+	
+	@RequestMapping(value="/upload.bo", method=RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public Response handleFileUpload(@RequestParam("file") MultipartFile[] file) {
+
+        List<String> result = new ArrayList<String>();
+        FileUpload fileUpload = new FileUpload();
+        for (int i = 0; i < file.length; i++) {
+            result.add(fileUpload.process(file[i]));
+        }
+
+        return new Response(result);
+    }
+
 }
