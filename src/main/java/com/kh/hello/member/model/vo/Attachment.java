062c7ba51ae6c7d7a3f63c12d26905faae63c79f
Administrator <Administrator@JINSEO-NT>
2018-10-02 13:08:41 +0900

    member fix

---
+++ src/main/java/com/kh/hello/member/model/vo/Attachment.java
@@ -0,0 +1,106 @@
+package com.kh.hello.member.model.vo;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Attachment implements java.io.Serializable{
+	private int fId;
+	private String filePath;
+	private String originName;
+	private String changeName;
+	private Date uploadDate;
+	private String aLevel;
+	private int refId;
+	private String status;
+	
+	public Attachment(){}
+
+	public Attachment(int fId, String filePath, String originName, String changeName, Date uploadDate, String aLevel,
+			int refId, String status) {
+		super();
+		this.fId = fId;
+		this.filePath = filePath;
+		this.originName = originName;
+		this.changeName = changeName;
+		this.uploadDate = uploadDate;
+		this.aLevel = aLevel;
+		this.refId = refId;
+		this.status = status;
+	}
+
+	public int getfId() {
+		return fId;
+	}
+
+	public void setfId(int fId) {
+		this.fId = fId;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getOriginName() {
+		return originName;
+	}
+
+	public void setOriginName(String originName) {
+		this.originName = originName;
+	}
+
+	public String getChangeName() {
+		return changeName;
+	}
+
+	public void setChangeName(String changeName) {
+		this.changeName = changeName;
+	}
+
+	public Date getUploadDate() {
+		return uploadDate;
+	}
+
+	public void setUploadDate(Date uploadDate) {
+		this.uploadDate = uploadDate;
+	}
+
+	public String getaLevel() {
+		return aLevel;
+	}
+
+	public void setaLevel(String aLevel) {
+		this.aLevel = aLevel;
+	}
+
+	public int getRefId() {
+		return refId;
+	}
+
+	public void setRefId(int refId) {
+		this.refId = refId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Attachment [fId=" + fId + ", filePath=" + filePath + ", originName=" + originName + ", changeName="
+				+ changeName + ", uploadDate=" + uploadDate + ", aLevel=" + aLevel + ", refId=" + refId + ", status="
+				+ status + "]";
+	}
+	
+	
+	
+}
