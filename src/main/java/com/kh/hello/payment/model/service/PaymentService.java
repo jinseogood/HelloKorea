231179ec90f55a667c5980794fdec6e9223f273b
Administrator <Administrator@JINSEO-NT>
2018-10-09 21:06:08 +0900

    js 1009 2

--- src/main/java/com/kh/hello/payment/model/service/PaymentService.java
+++ src/main/java/com/kh/hello/payment/model/service/PaymentService.java
@@ -10,4 +10,10 @@ public interface PaymentService {
 	//결제 정보 등록
 	int insertAllPayment(Payment p, ArrayList<PayDetail> pdList);
 
+	//결제 완료 페이지에 보여 줄 결제 정보 조회
+	Payment selectPayInfo(int mId);
+	
+	//결제 완료 페이지에 보여 줄 결제 상세 정보 조회
+	ArrayList<PayDetail> selectPayDetailInfo(int paId);
+
 }
