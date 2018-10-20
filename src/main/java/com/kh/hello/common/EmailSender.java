109ca1b54f3c1ff3c260ea2037d7a71a7be656eb
Administrator <Administrator@JINSEO-NT>
2018-10-21 03:43:58 +0900

    js 1021 1

--- src/main/java/com/kh/hello/common/EmailSender.java
+++ src/main/java/com/kh/hello/common/EmailSender.java
@@ -2,25 +2,35 @@ package com.kh.hello.common;
 
 import java.io.StringWriter;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class EmailSender {
 	
+	@Autowired
+	private JavaMailSender nMailSender;
 	
 	@Autowired
 	private MailSender mailSender;
 	
 	@Autowired
 	private VelocityEngine velocityEngine;
+	
+	public static final String mailPath="/resources/templates/";
 	
 	public void setMailSender(MailSender mailSender) {
 		this.mailSender = mailSender; 
@@ -30,6 +40,26 @@ public class EmailSender {
 		this.velocityEngine = velocityEngine; 
 	}
 	
+	//일반 메일 전송
+	public void sendMailNormal(Email mail) throws Exception{
+		MimeMessage msg=nMailSender.createMimeMessage();
+		
+		try{
+			msg.setSubject(mail.getMailSubject());
+			
+			msg.setText(mail.getMailContent());
+			msg.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(mail.getMailTo()));
+		}
+		catch(MessagingException e){
+			System.out.println(e.getMessage());
+		}
+		catch(MailException e2){
+			System.out.println(e2.getMessage());
+		}
+		
+	}
+	
+	//템블릿 메일 전송
 	public void sendMail(Email mail, int orderNum, String orderDate, String orderName, String orderPhone, String proCName, String proRName, double proPrice, HttpServletRequest request) {
 		
 		SimpleMailMessage message = new SimpleMailMessage();
@@ -37,13 +67,11 @@ public class EmailSender {
 		message.setTo(mail.getMailTo());
 		message.setSubject(mail.getMailSubject());
 		
-		String root=request.getSession().getServletContext().getRealPath("resources");
-		
-		String filePath=root + "\\templates\\";
+		String filePath=mailPath + mail.getTemplateName();
 		
 		System.out.println("filePath : " + filePath);
 		
-		Template template = velocityEngine.getTemplate(filePath + mail.getTemplateName());
+		Template template = velocityEngine.getTemplate(mail.getTemplateName());
 		VelocityContext velocityContext = new VelocityContext();
 		velocityContext.put("orderNum", orderNum);
 		velocityContext.put("orderDate", orderDate);
